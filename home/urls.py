249cb2a209d8297f5a10f26342ff61b94e35460a
Pranjal0512 <pranjal.barnwal051234@gmail.com>
2023-02-02 22:23:26 +0545

    first commit

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,12 @@ from django.urls import path
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('cart', HomeView.as_view(), name='cart'),
+    path('checkout', HomeView.as_view(), name='checkout'),
+    path('contact', HomeView.as_view(), name='contact'),
+    path('login', HomeView.as_view(), name='login'),
+    path('my-account', HomeView.as_view(), name='my-account'),
+    path('product-detail', HomeView.as_view(), name='product-detail'),
+    path('product-list', HomeView.as_view(), name='product-list'),
+    path('wishlist', HomeView.as_view(), name='wishlist'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,20 +6,20 @@ from .models import *
 # Create your views here.
 class BaseView(View):
     views = {}
-    views['categories'] = Category.object.all()
+    views['categories'] = Category.objects.all()
 
 
-class HomeView(View):
+class HomeView(BaseView):
     def get(self, request):
-        self.views['categories']= Category.object.all()
-        self.views['subcategories']= SubCategory.object.all()
-        self.views['sliders']= Slider.object.all()
-        self.views['ads']= Ad.object.all()
-        self.views['brands']= Brand.object.all()
-        self.views['reviews']= Review.object.all()
-        self.views['news_product']= Product.object.filter(labels='new')
-        self.views['hot_product']= Product.object.filter(labels='hot')
-        self.views['sale_product']= Product.object.filter(labels='sale')
+        self.views['categories']= Category.objects.all()
+        self.views['subcategories']= SubCategory.objects.all()
+        self.views['sliders']= Slider.objects.all()
+        self.views['ads']= Ad.objects.all()
+        self.views['brands']= Brand.objects.all()
+        self.views['reviews']= Review.objects.all()
+        self.views['news_product']= Product.objects.filter(labels='new')
+        self.views['hot_product']= Product.objects.filter(labels='hot')
+        self.views['sale_product']= Product.objects.filter(labels='sale')
 
 
         return render(request, 'index.html', self.views)
